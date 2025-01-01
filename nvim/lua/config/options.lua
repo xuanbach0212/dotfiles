8645083525bbf59391e12f27404c40dec3f89d0b
DAOXUAN BACH <xuanbachdao0212@gmail.com>
2025-01-01 16:43:04 +0900

    update luaconfig

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -15,3 +15,11 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.undofile = true
+
+-- Indentation - use tabs
+vim.opt.expandtab = false      -- Use tabs instead of spaces
+vim.opt.shiftwidth = 4         -- Indent width (4 spaces display)
+vim.opt.tabstop = 4            -- Tab width (4 spaces display)
+vim.opt.softtabstop = 4        -- Backspace removes 4 spaces
+vim.opt.smartindent = true     -- Auto-indent new lines
+vim.opt.autoindent = true      -- Copy indent from current line
---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        c = false,
+        cpp = false,
+        h = false,
+        hpp = false,
+        ["*"] = true,
+      },
+    },
+  },
+}
