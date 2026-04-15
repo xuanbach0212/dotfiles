0e3ba511c9776927bf9c87a980c8b64b7802a3e4
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-15 18:07:27 +0900

    remove preselect

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = false, -- Don't auto-select first item, Enter = new line
+        },
+      },
+    },
+  },
+}
