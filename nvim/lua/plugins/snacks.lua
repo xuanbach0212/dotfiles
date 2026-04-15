c73247e6a2bc40a2e84fbaf67edbd58a07adb135
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-15 17:07:06 +0900

    add scratch

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,9 +4,10 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     explorer = { enabled = false },
+    scratch = { enabled = true },
+  },
+  keys = {
+    { "<leader>bs", function() Snacks.scratch() end, desc = "Scratch Buffer" },
   },
 }
