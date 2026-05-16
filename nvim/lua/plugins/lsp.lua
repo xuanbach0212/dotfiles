73d966c3c88a42e93186411c0c18bc306d2869ce
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-05-16 21:40:25 +0700

    basedpyright noti so much

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,6 +8,14 @@ return {
       },
       servers = {
         basedpyright = {
+          handlers = {
+            ['$/progress'] = function(err, result, ctx)
+              if result.token == (vim.g.basedpyright_progress_token or result.token) then
+                vim.g.basedpyright_progress_token = result.token
+                vim.lsp.handlers['$/progress'](err, result, ctx)
+              end
+            end,
+          },
           settings = {
             basedpyright = {
               analysis = {
